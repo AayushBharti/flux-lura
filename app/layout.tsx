b72a01ce370704c174c999c3093b8502187055fd
aayushbharti <iaayushbharti@gmail.com>
2024-10-14 19:04:14 +0530

    added Toaster and updated metadata in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next"
 import { ThemeProvider } from "@/components/theme-provider"
 import localFont from "next/font/local"
 import "./globals.css"
+import { Toaster } from "../components/ui/toaster"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -15,14 +16,12 @@ const geistMono = localFont({
 })
 
 export const metadata: Metadata = {
-  title: "Flux Lura - Free Unlimited File Converter",
-  description: `Unleash your creativity with Flux Lura – the ultimate online tool for
-unlimited and free multimedia conversion. Transform images, audio, and
-videos effortlessly, without restrictions. Start converting now and
-elevate your content like never before!`,
-  creator: "SOUHAIL BEN-LHACHEMI",
+  title: "Flux Lura - Your Unlimited Free File Converter",
+  description:
+    "Unlock your creative potential with Flux Lura, the premier online tool for seamless multimedia conversion. Effortlessly transform images, audio, and videos without any limitations. Experience the freedom of unlimited conversions and elevate your content like never before. Start your journey with Flux Lura today!",
+  creator: "Aayush Bharti",
   keywords:
-    "image converter, video converter, audio converter, unlimited image converter, unlimited video converter",
+    "image converter, video converter, audio converter, unlimited image converter, unlimited video converter, unlimited file conversion, multimedia transformation",
 }
 
 export default function RootLayout({
@@ -36,7 +35,10 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          {children}
+          <Toaster />
+          <div className="max-w-4xl min-h-screen pt-28 lg:pt-32 2xl:pt-40 lg:max-w-6xl 2xl:max-w-7xl mx-auto">
+            {children}
+          </div>
         </ThemeProvider>
       </body>
     </html>
