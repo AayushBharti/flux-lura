bdd79381cf63ec8f1bfe92bf5405edba8ef7f443
AayushBharti <iaayushbharti@gmail.com>
2024-10-18 16:05:55 +0530

    added fileToIcon utitlity function

--- utils/file-to-icon.tsx
+++ utils/file-to-icon.tsx
@@ -1,12 +1,13 @@
 // imports
-import { FileAudio, FileText, Image, File, Video } from "lucide-react"
+import { FileText, Image, Video, Music, FileIcon } from "lucide-react"
 
-
-
-export default function fileToIcon( fileType:string ) {
-  if (fileType.includes("video")) return <Video />
-  if (fileType.includes("audio")) return <FileAudio />
-  if (fileType.includes("text")) return <FileText />
-  if (fileType.includes("image")) return <Image />
-  return <File />
+export default function fileToIcon(fileType: string) {
+  // if (fileType.includes("text")) return <FileText />
+  if (fileType.includes("image"))
+    return <Image className="text-blue-500" size={24} />
+  if (fileType.includes("video"))
+    return <Video className="text-green-500" size={24} />
+  if (fileType.includes("audio"))
+    return <Music className="text-yellow-500" size={24} />
+  return <FileIcon className="text-gray-500" size={24} />
 }
